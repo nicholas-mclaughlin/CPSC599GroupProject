1700a74f2773fcafd2d74d1323227ed388c80ab7
LanJinUC <46972646+LanJinUC@users.noreply.github.com>
2021-04-13 01:30:01 -0600

    Update and rename sketch_one_channel.py to sketchCNN.py

--- sketchCNN.py
+++ sketchCNN.py
@@ -13,10 +13,10 @@ canvas_height = 500
 
 # https://www.semicolonworld.com/question/55284/how-can-i-convert-canvas-content-to-an-image
 # These are used to draw the image to memory at the same time it is also drawn on the canvas
-image = Image.new("1", (canvas_width, canvas_height), (255))
+image = Image.new("L", (canvas_width, canvas_height), (255))
 draw = ImageDraw.Draw(image)
-
-file_path =  "/Users/lanjin/Desktop/CPSC599GroupProject/model/cnn-2.h5"
+#This should be the absolute path to the CNN model
+file_path =  "/Users/lanjin/Desktop/CPSC599GroupProject/model/cnn_model.h5"
 
 # Load model
 loaded_model = tf.keras.models.load_model(file_path)
