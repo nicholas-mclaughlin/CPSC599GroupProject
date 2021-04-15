8448ac7b8c542ae61e0c36326e14b40402356916
Dustin Doell <doelldd@gmail.com>
2021-04-15 17:24:59 -0600

    Update trainTransferPartial.py

--- trainTransferPartial.py
+++ trainTransferPartial.py
@@ -10,25 +10,32 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 # import warnings
 # warnings.filterwarnings("ignore")
 
+# Setup variables
 img_rows, img_cols = 28, 28
-model_name = "modelTransferPartial.h5"
+model_name = "modelTransferPartial"
+num_samples_per_class = 3000
+num_classes = 10
+epochs = 10
 
 # Determine data directory
-data_dir = os.path.join(sys.path[0], "data")
-model_dir = os.path.join(sys.path[0], "model")
+cwd = os.getcwd()
+data_dir = os.path.join(cwd, "data_reduced")
+model_dir = os.path.join(cwd, "model")
 print("Data Directory: {}".format(data_dir))
 
+# Look at the data directory files, get a list of files in the directory,
+# reduce to the first 10 files, parse the labels from the file names
 FILES = os.listdir(data_dir)
 FILES = [x for x in FILES if x.endswith('.npy')]
 FILES = sorted(FILES)
-FILES = FILES[0:10]
+FILES = FILES[0:num_classes]
 LABELS = [file.replace(".npy", "") for file in FILES]
-num_classes = len(LABELS)
 
-# Dataset with where each entry is flattened numpy image
+# Create empty dataset with where each entry is flattened numpy image
 data = np.array([]).reshape(0, img_rows * img_cols)
 target = np.array([]).reshape(0, 1)
 
+# Loop through loading numpy files and set up dataset and target arrays
 i = 0
 for numpy_file in FILES:
     print("--> Loading the numpy file: {}".format(numpy_file))
@@ -39,8 +46,9 @@ for numpy_file in FILES:
     target_i = np.ones(data_num_samples, dtype=int) * i
     target_i = target_i[:, np.newaxis]
 
-    data = np.append(data, data_i[8000:11000], axis=0)
-    target = np.append(target, target_i[8000:11000], axis=0)
+    # Append current data to combined array
+    data = np.append(data, data_i[:num_samples_per_class], axis=0)
+    target = np.append(target, target_i[:num_samples_per_class], axis=0)
 
     i += 1
     print("Current data array shape: {}".format(data.shape))
@@ -52,13 +60,9 @@ print("Final target array shape: {}".format(target.shape))
 
 # Split into train and test sets
 x_train, x_test, y_train, y_test = train_test_split(data, target, stratify=target, test_size=0.25, random_state=0)
+print("_________________________")
 print("Train Length: {} images".format(len(x_train)))
 print("Test Length: {} images".format(len(x_test)))
-
-# for i in range(10):
-#     plt.imshow(x_train[i].reshape(28,28))
-#     plt.title(LABELS[(int)(y_train[i])])
-#     plt.show()
 
 # Normalize image data
 x_train = x_train.astype('float32')
@@ -78,71 +82,70 @@ x_test = np.repeat(x_test,3,3)
 y_train_onehot = keras.utils.to_categorical(y_train, num_classes)
 y_test_onehot = keras.utils.to_categorical(y_test, num_classes)
 
+print("_________________________")
 print("Train Sample Shape: {}".format(x_train.shape))
 print("Train Label Shape: {}".format(y_train_onehot.shape))
+print("_________________________")
 
-def binarize(image):
-    image[image >= 0.5] = 1
-    image[image < 0.5] = 0
-    return image
-
+# Define image augmentation
 datagen = ImageDataGenerator(
     rotation_range=30,
     width_shift_range=0.1,
     height_shift_range=0.1,
     shear_range=0.05,
-    # preprocessing_function=binarize,
     fill_mode='nearest')
 
 datagen.fit(x_train)
 
-def transferPartialTrainable():
+def trainTransferPartialTrainable():
     # Transfer learning Mobilenet model with no layers trainable
     from tensorflow.keras.optimizers import Adam
     from tensorflow.keras import layers
     from tensorflow.keras import Model
     from tensorflow.keras.applications.mobilenet_v2 import MobileNetV2
 
+    # Base model is a MobileNet with ImageNet pretrained weights and the top removed
     base_model = MobileNetV2(input_shape=(32,32,3),weights='imagenet',classes=num_classes,include_top=False)
-    base_model.summary()
 
+    # Set some of the layers to be trainable
     for layer in base_model.layers:
         if layer.name == 'block_15_expand':
             print('break')
             break
         layer.trainable=False
 
-    last_layer = base_model.get_layer('out_relu')
-    print('last layer output shape: ', last_layer.output_shape)
-    last_output = last_layer.output
-
+    # Define model using functional method, input -> resize -> MobileNet
     input_layer = keras.layers.Input(shape=(28,28,3))
     x = keras.layers.Layer()(input_layer)
-    print(x)
     x = layers.experimental.preprocessing.Resizing(32,32,interpolation="bilinear",name="the_resize_layer")(x)
     x = base_model(x)
-    # Flatten the output layer to 1 dimension
+    # Flatten the output layer
     x = layers.GlobalAveragePooling2D()(x)
     # Add a fully connected layer with 1,024 hidden units and ReLU activation
     x = layers.Dense(1024, activation='relu')(x)
     # Add a final sigmoid layer for classification
     output = layers.Dense(10, activation='sigmoid')(x)
-
     model = Model(inputs=input_layer, outputs=output)
-    # model.summary()
 
+    # Compile model
     model.compile(optimizer = Adam(lr=0.0001), 
                   loss = 'categorical_crossentropy', 
                   metrics = ['accuracy'])
 
+    # Train model with samples flowing from ImageDataGenerator
     model.fit_generator(datagen.flow(x_train, y_train_onehot, batch_size=10),
                         steps_per_epoch=len(x_train)//10,
-                        epochs = 10,
+                        epochs = epochs,
                         verbose = 1
     )
     return model
 
-model = transferPartialTrainable()
+# Call training function
+model = trainTransferPartialTrainable()
+
+# Validate on test dataset
+score = model.evaluate(x_test, y_test_onehot, verbose = 1)
+print("\nAccuracy on Test Set: {}".format(score[1]))
 
 # Save model
 model.save(os.path.join(model_dir, model_name + ".h5"))
